28b0e1a25eaafe1387bec6203cbe283bc4bb2881
unknown <gustavocastruu@gmail.com>
2026-01-27 22:46:28 -0300

    Iniciando com o playwright

--- automatizacao/pw/teste-pw.py
+++ automatizacao/pw/teste-pw.py
@@ -1,1 +1,12 @@
-import 
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as p:
+    navegador = p.chromium.launch(headless=False)
+    page = navegador.new_page()
+
+    page.goto("https://www.youtube.com/")
+
+    print(page.title()) 
+
+navegador.close()
